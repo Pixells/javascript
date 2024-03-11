d66a285aa494c7ef96e99d13d93d1830bffdf92a
Erik Matheus <80124753+Pixells@users.noreply.github.com>
2024-03-11 15:54:25 -0300

    aula12 ex011, 012

---
+++ CursoJS/aula12/ex012.js
@@ -0,0 +1,15 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} Horas.`)
+
+if (hora < 12) {
+    console.log(`Bom Dia!`)
+}
+else{
+    if (hora <= 18){
+        console.log(`Boa Tarde!`)
+    }
+    else{
+        console.log(`Bom Noite`)
+    }
+}
