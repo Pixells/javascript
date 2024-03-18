94b25e209fc6a0d83055d6d5cef84b40ecff8d2e
Erik Matheus <80124753+Pixells@users.noreply.github.com>
2024-03-18 12:33:44 -0300

    aula14 estudando repetição com for

---
+++ CursoJS/aula14/ambiente.js
@@ -0,0 +1,26 @@
+console.log(`Vai começar...`)
+for (var c = 1; c <= 6; c++){
+    console.log(c)
+}
+console.log(`FIM!`)
+
+
+//--------------------------------
+/*
+
+
+for (var c = 1; c <= 6; c++){
+    console.log(`Passo ${c}`)
+}
+
+
+----------------------------------
+
+var c = 1
+
+while (c <= 6 ) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+*/
